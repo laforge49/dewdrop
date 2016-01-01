d205e3b7cfeb3881548298a0a41653787897f4fd
Bill La Forge <laforge49@gmail.com>
2016-01-01 07:43:21 -0500

    version 0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject dewdrop "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject dewdrop "0.1.0"
+  :description "lenses made simple"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
